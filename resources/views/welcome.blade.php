bdf879ef5f750317f5f2400589423664850d2306
GeraldJohn1234 <geraldjohnhiponia@gmail.com>
2022-03-04 15:43:00 +0800

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Inventory</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -17,4 +17,4 @@
         </div>
        <script src="{{ mix('js/app.js') }}"></script>
     </body>
-</html>
+</html> --}}
